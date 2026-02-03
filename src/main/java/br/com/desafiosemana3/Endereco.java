a3cab4de6d00e36bbeedbec09da46c2bec04b600
Gerenciamento-de-Veiculos <pedrohenriquemenezes76@gmail.com>
2026-02-03 17:16:14 -0300

    Semana 3 Dia 02: mapeamento de JSON para objeto Endereco com Gson

---
+++ src/main/java/br/com/desafiosemana3/Endereco.java
@@ -0,0 +1,30 @@
+package br.com.desafiosemana3;
+
+public class Endereco {
+
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+}
--- src/main/java/br/com/desafiosemana3/Main.java
+++ src/main/java/br/com/desafiosemana3/Main.java
@@ -1,5 +1,6 @@
 package br.com.desafiosemana3;
 
+import com.google.gson.Gson;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,13 +12,11 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Digite um CEP para consulta: ");
         String cep = scanner.nextLine();
 
         try {
             HttpClient client = HttpClient.newHttpClient();
-
             String url = "https://viacep.com.br/ws/" + cep + "/json/";
 
             HttpRequest request = HttpRequest.newBuilder()
@@ -28,8 +27,15 @@ public class Main {
             HttpResponse<String> response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            System.out.println("\nResposta da API:");
-            System.out.println(response.body());
+            Gson gson = new Gson();
+            Endereco endereco = gson.fromJson(response.body(), Endereco.class);
+
+            System.out.println("\nEndereço encontrado:");
+            System.out.println("CEP: " + endereco.getCep());
+            System.out.println("Logradouro: " + endereco.getLogradouro());
+            System.out.println("Bairro: " + endereco.getBairro());
+            System.out.println("Cidade: " + endereco.getLocalidade());
+            System.out.println("UF: " + endereco.getUf());
 
         } catch (Exception e) {
             System.out.println("Erro ao consultar o CEP.");
